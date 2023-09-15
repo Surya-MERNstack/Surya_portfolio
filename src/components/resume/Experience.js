587e798b90faa5df793963a4dd8f97642e5a290e
Surya-MERNstack <charusurya17@gmail.com>
2023-09-15 09:19:37 +0530

    add

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import ResumeCard from "./ResumeCard";
 
 const Experience = () => {
@@ -10,7 +10,6 @@ const Experience = () => {
       className="py-12 font-titleFont flex gap-20"
     >
       <div>
-      
         <div className="mt-14 w-full h-auto ">
           <ResumeCard
             title="Fresher"
@@ -18,7 +17,9 @@ const Experience = () => {
             result="Waiting"
             des="If I given opportunity to work your company, I will do my Best"
           />
-         
+          <a href="https://dochub.com/hulkcharlie17/ALzmZB7wM1XQ4j0wX8J560/surya-mern-stack-resume-pdf?mode=view">
+            <ResumeCard title="SuryaPerumal" subTitle="Resume" />
+          </a>
         </div>
       </div>
     </motion.div>
--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,18 @@
-import { FaGlobe} from "react-icons/fa";
-import { SiProgress } from "react-icons/si";
-
+import { FaGlobe } from "react-icons/fa";
+import {HiOutlineDownload} from "react-icons/hi"
 // Features Data
 export const featuresData = [
   {
     id: 1,
     icon: <FaGlobe />,
     title: "Web Development",
-     des : "Developing and designing front end web architecture and building interactive consumer data from multiple systems."
+    des: "Developing and designing front end web architecture and building interactive consumer data from multiple systems.",
+  },
+  {
+    id : 2,
+    icon : <HiOutlineDownload/>,
+    title : "Resume",
+    des : "MernStack",
+    link : <a href="https://dochub.com/hulkcharlie17/ALzmZB7wM1XQ4j0wX8J560/surya-mern-stack-resume-pdf?mode=view"></a>
   }
 ];
